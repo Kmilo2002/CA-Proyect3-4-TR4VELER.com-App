e7a0bf588c87e4c1a16a848ac6bbd6047411d16b
Kmilo <camilotellez2002@gmail.com>
2023-03-17 15:32:13 +0100

    update

--- routes/ReservationsRouter.js
+++ routes/ReservationsRouter.js
@@ -1,203 +1,219 @@
 const express = require("express");
 const ReservationsRouter = express.Router();
-const Reservations = require("../models/Reservations")
+const Reservations = require("../models/Reservations");
 const User = require("../models/User");
-const Logging = require("../models/Logging")
+const Logging = require("../models/Logging");
 const auth = require("../middleware/auth");
 const authAdmin = require("../middleware/authAdmin");
 
 let myReservation;
 
-ReservationsRouter.post("/register/reservation", auth, async (req, res) =>{
-    const { days, persons, meals, loggingId, paymentId } = req.body;
-    try {
-     let daysFind = await Reservations.findOne({days});
-     if(daysFind){
-        return res.status(400).send({
+ReservationsRouter.post("/register/reservation", auth, async (req, res) => {
+  const { days, persons, meals, loggingId, paymentId } = req.body;
+  try {
+    let daysFind = await Reservations.findOne({ days });
+    if (daysFind) {
+      return res.status(400).send({
         success: false,
-        message:
-          "¡Fechas no disponibles!",
+        message: "¡Fechas no disponibles!",
       });
-     }
-     if( !days|| !persons|| !meals|| !loggingId|| !paymentId ){
-        return res.status(400).send({
-        success: false,
-        message:
-          "No ha llenado todas las características de su estadía!",
-      });
-     }
-     
-     myReservation = new Reservations({
-        days,
-        persons,
-        meals,
-        user: req.user.id,
-        logging: loggingId
-     });
-
-     await User.findByIdAndUpdate(req.user.id, {
-      $push:{
-        reservation: myReservation._id
-      }
-     })
-
-     await Logging.findByIdAndUpdate(req.logging.id, {
-      $push:{
-        reservation: myReservation._id
-      }
-     })
-
-     await myReservation.save();
-
-     return res.status(201).send({
-        success: true,
-        message: "¡Reservación creada correctamente!",
-        myReservation,
-      });
-
-    } catch (error) {
-        return res.status(500).send({
-            success: false,
-            message: error.message,
-          });
     }
+    if (!days || !persons || !meals || !loggingId || !paymentId) {
+      return res.status(400).send({
+        success: false,
+        message: "No ha llenado todas las características de su estadía!",
+      });
+    }
+
+    myReservation = new Reservations({
+      days,
+      persons,
+      meals,
+      user: req.user.id,
+      logging: loggingId,
+    });
+
+    await User.findByIdAndUpdate(req.user.id, {
+      $push: {
+        reservation: myReservation._id,
+      },
+    });
+
+    await Logging.findByIdAndUpdate(req.logging.id, {
+      $push: {
+        reservation: myReservation._id,
+      },
+    });
+
+    await myReservation.save();
+
+    return res.status(201).send({
+      success: true,
+      message: "¡Reservación creada correctamente!",
+      myReservation,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      success: false,
+      message: error.message,
+    });
+  }
 });
 
 ReservationsRouter.get("/reservations", auth, authAdmin, async (req, res) => {
   try {
     let reservas = await Reservations.find({});
-    if(!reservas){
+    if (!reservas) {
       return res.status(404).send({
         success: false,
-        message: "¡There is no reservations in DB!"
-      })
+        message: "¡There is no reservations in DB!",
+      });
     }
     return res.status(200).send({
       success: true,
-      reservas
-    })
+      reservas,
+    });
   } catch (error) {
     return res.status(500).send({
       success: false,
       message: error.message,
     });
   }
-})
+});
 
-ReservationsRouter.get("/reservations/:id", auth, authAdmin, async (req, res) => {
-  try {
-    const {id} = req.params;
-    //let reservation = await Reservations.findById(id).select("User").populate("user")
-    let reservation = await Reservations.findById(id).select("User Logging").populate({path:"user", select:"name surname email phone"}).populate({path:"logging", select:"name title"})
-    if(!reservation){
-      return res.status(404).send({
-        sucess: false,
-        message: "¡There is no reservation with that id"
-      })
+ReservationsRouter.get(
+  "/reservations/:id",
+  auth,
+  authAdmin,
+  async (req, res) => {
+    try {
+      const { id } = req.params;
+      //let reservation = await Reservations.findById(id).select("User").populate("user")
+      let reservation = await Reservations.findById(id)
+        .select("User Logging")
+        .populate({ path: "user", select: "name surname email phone" })
+        .populate({ path: "logging", select: "name title" });
+      if (!reservation) {
+        return res.status(404).send({
+          sucess: false,
+          message: "¡There is no reservation with that id",
+        });
+      }
+      return res.status(200).send({
+        success: true,
+        reservation,
+      });
+    } catch (error) {
+      return res.status(500).send({
+        success: false,
+        message: error.message,
+      });
     }
-    return res.status(200).send({
-      success: true,
-      reservation
-    })
-  } catch (error) {
-    return res.status(500).send({
-      success: false,
-      message: error.message
-    })
   }
-})
+);
 
 ReservationsRouter.get("/user_reservations", auth, async (req, res) => {
   try {
-    let reservations = await User.findById(req.user.id).select("reservation").populate({})
-    if(!reservations){
+    let reservations = await User.findById(req.user.id)
+      .select("reservation")
+      .populate({});
+    if (!reservations) {
       return res.status(404).send({
         success: false,
-        message: "No reservations found!"
-      })
+        message: "No reservations found!",
+      });
     }
     return res.status(200).send({
       success: true,
-      reservations
-    })
+      reservations,
+    });
   } catch (error) {
     return res.status(500).send({
       success: false,
-      message: error.message
-    })
+      message: error.message,
+    });
   }
-})
+});
 
 ReservationsRouter.put("/reservations_modify/:id", auth, async (req, res) => {
   try {
-    const {id} = req.params;
-    const {days, persons, meals} = req.body;
-    let reservations = await Reservations.findByIdAndUpdate(id, {days, persons, meals})
-    if(!id){
+    const { id } = req.params;
+    const { days, persons, meals } = req.body;
+    let reservations = await Reservations.findByIdAndUpdate(id, {
+      days,
+      persons,
+      meals,
+    });
+    if (!id) {
       return res.status(404).send({
         success: false,
-        message: "There is no reservation with that id"
-      })
+        message: "There is no reservation with that id",
+      });
     }
     return res.status(200).send({
       success: true,
       message: "Reservation Updated",
-      reservations
-    })
+      reservations,
+    });
   } catch (error) {
     return res.status(500).send({
       success: false,
       message: error.message,
     });
   }
-})
+});
 
 ReservationsRouter.delete("/reservations/:id", auth, async (req, res) => {
   try {
-    const {id} = req. params;
+    const { id } = req.params;
     await User.findByIdAndUpdate(req.user.id, {
-      $pull:{
-        reservation: id
-      }
-    })
+      $pull: {
+        reservation: id,
+      },
+    });
     await Reservations.findByIdAndDelete(id);
-    if(!id){
+    if (!id) {
       res.status(404).send({
         success: false,
-        message: "Reservation not found!"
-      })
+        message: "Reservation not found!",
+      });
     }
     return res.status(200).send({
       sucess: true,
-      message: "Reservation deleted correctly!"
-    })
+      message: "Reservation deleted correctly!",
+    });
   } catch (error) {
     return res.status(500).send({
       success: false,
       message: error.message,
     });
   }
-})
+});
 
-ReservationsRouter.delete("/reservations/:id", auth, authAdmin, async (req, res) => {
-  try {
-    const {id} = req. params;
-    await Reservations.findByIdAndDelete(id);
-    if(!id){
-      res.status(404).send({
+ReservationsRouter.delete(
+  "/reservations/:id",
+  auth,
+  authAdmin,
+  async (req, res) => {
+    try {
+      const { id } = req.params;
+      await Reservations.findByIdAndDelete(id);
+      if (!id) {
+        res.status(404).send({
+          success: false,
+          message: "Reservation not found!",
+        });
+      }
+      return res.status(200).send({
+        sucess: true,
+        message: "Reservation deleted correctly!",
+      });
+    } catch (error) {
+      return res.status(500).send({
         success: false,
-        message: "Reservation not found!"
-      })
+        message: error.message,
+      });
     }
-    return res.status(200).send({
-      sucess: true,
-      message: "Reservation deleted correctly!"
-    })
-  } catch (error) {
-    return res.status(500).send({
-      success: false,
-      message: error.message,
-    });
   }
-})
+);
 module.exports = ReservationsRouter;
